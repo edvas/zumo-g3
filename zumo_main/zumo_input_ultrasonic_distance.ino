6e8b5c3a44dd386d9798bb631aad4717fff69e66
Edvard Ekrem Sæther <44024237+edvas@users.noreply.github.com>
2021-12-11 10:39:05 +0100

    Update zumo_input_ultrasonic_distance.ino

--- zumo_main/zumo_input_ultrasonic_distance.ino
+++ zumo_main/zumo_input_ultrasonic_distance.ino
@@ -6,7 +6,7 @@ uint8_t UDI1_value_cm = 0;
 
 void ReadUltrasonicDistanceInputs()
 { 
-  // Clears the trigPin
+  // Clears the trigger pin
   DDRC |= (1 << 0);
   PORTC &= ~(1 << 0);
   _delay_us(2);
@@ -15,13 +15,13 @@ void ReadUltrasonicDistanceInputs()
   PORTC &= ~(1 << 0);
   DDRC &= ~(1 << 0);
 
-  // Reads the echoPin, returns the sound wave travel time in microseconds
+  // Reads the echo pin, returns the sound wave travel time in microseconds
   uint16_t duration = pulseIn(A0, HIGH, kUltrasonicTimeout);
   // Calculating the distance
   UDI0_value_cm = (duration / kUltrasonicTimeToDistanceDivisor) - 1;
 
 
-  // Clears the trigPin
+  // Clears the trigger pin
   DDRC |= (1 << 1);
   PORTC &= ~(1 << 1);
   _delay_us(2);
@@ -30,7 +30,7 @@ void ReadUltrasonicDistanceInputs()
   PORTC &= ~(1 << 1);
   DDRC &= ~(1 << 1);
 
-  // Reads the echoPin, returns the sound wave travel time in microseconds
+  // Reads the echo pin, returns the sound wave travel time in microseconds
   duration = pulseIn(A1, HIGH, kUltrasonicTimeout);
   // Calculating the distance
   UDI1_value_cm = (duration / kUltrasonicTimeToDistanceDivisor) - 1;
